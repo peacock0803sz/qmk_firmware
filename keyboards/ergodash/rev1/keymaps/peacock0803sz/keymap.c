584707e63b8a1a4d429e54178583b7ebed7d403f
peacock0803sz <peacock0803sz@gmail.com>
2019-08-07 04:22:13 +0900

    :bookmark: Version 1

--- keyboards/ergodash/rev1/keymaps/peacock0803sz/keymap.c
+++ keyboards/ergodash/rev1/keymaps/peacock0803sz/keymap.c
@@ -32,28 +32,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * +----------------------------------------------------------------------------------------------------------------------+
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,                         KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS,                        KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,                        KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_MINS,                        KC_EQL,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_GRV,                         KC_GRV,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,                         KC_ENT,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    EISU,    EISU,    KC_LGUI, KC_LALT,          LOWER,   KC_ENT, KC_SPC,         KC_SPC, KC_ENT,  RAISE,            KC_RALT, KC_RCTL, EISU,    EISU     \
+    EISU,    EISU,    KC_LGUI, KC_LALT,          LOWER,   KC_ENT,  KC_SPC,        KC_SPC, KC_ENT,  RAISE,            KC_RALT, KC_RCTL, EISU,    EISU     \
   ),
 
   /* Lower
    * +----------------------------------------------------------------------------------------------------------------------+
-   * |   `  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  Del |
+   * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |  Tab |PGDown|  Up  | PGUp | Home | Stop |Mouse4|                    | Prev |Mouse1|Mouse3|Mouse2| PScr | VolUp| Alt+\|
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Caps | Left | Down | Right|  End | Play |Mouse5|                    | Next | MLeft|  MUp | MDown|MRight| Mute |  Mic |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift| Undo |  Cut | Copy | Paste| Redo | Space|                    | Enter|  MWL |  MWU |  MWD |  MWR | VolDn| Shift|
+   * | Shift| Undo |  Cut | Copy | Paste| Redo | Enter|                    | Enter|  MWL |  MWU |  MWD |  MWR | VolDn| Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | EISU | EISU |  GUI |  Alt |||||||| Lower| Enter| Space||||||||Soace | Enter| Raise|||||||| Alt  |RCtrl | EISU | EISU |
    * +----------------------------------------------------------------------------------------------------------------------+
    */
-  [_LOWER] = LAYOUT(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
+  [_RAISE] = LAYOUT(
+    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
     KC_TAB,  KC_UGUP, KC_UP,   KC_HASH, KC_HOME, KC_MSTP, KC_UNDS,                        KC_MPRV, KC_BTN1, KC_BTN3, KC_BTN2, KC_PSCR, KC_VOLU, KC_MAIL, \
     KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_MPLY, KC_BTN4,                        KC_MNXT, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_MUTE, KC_CALC, \
     KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_BTN5,                        KC_ENT,  KC_MH_L, KC_MH_U, KC_MH_D, KC_MH_R, KC_VOLD, KC_RSFT, \
@@ -62,19 +62,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Raise
    * +----------------------------------------------------------------------------------------------------------------------+
-   * |  F11 |  F1  |  F2  |  F3  |  F4  |  F5  |   {  |                    |   }  |  F6  |  F7  |  F8  |  F9  |  F10 |  F12 |
+   * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |  Del |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * |  ~   |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
+   * |  Tab |PGDown|  Up  | PGUp | Home | Stop |Mouse4|                    | Prev |Mouse1|Mouse3|Mouse2| PScr | VolUp| Alt+\|
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Tab  |   1  |   2  |   3  |   4  |   5  |  Del |                    | Bksp |   H  |   J  |   K  |   L  |   :  |  "   |
+   * | Caps | Left | Down | Right|  End | Play |Mouse5|                    | Next | MLeft|  MUp | MDown|MRight| Mute |  Mic |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   6  |   7  |   8  |   9  |   0  | Space|                    | Enter|   N  |   M  |   <  |   >  |   ?  | Shift|
+   * | Shift| Undo |  Cut | Copy | Paste| Redo | Enter|                    | Enter|  MWL |  MWU |  MWD |  MWR | VolDn| Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | EISU |||||||| Lower| Space|  Del |||||||| Bksp | Enter| Raise|||||||| Home |PageDn|PageUp|  End |
+   * | EISU | EISU |  GUI |  Alt |||||||| Lower| Enter| Space||||||||Soace | Enter| Raise|||||||| Alt  |RCtrl | EISU | EISU |
    * +----------------------------------------------------------------------------------------------------------------------+
    */
-  [_RAISE] = LAYOUT(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
+[_RAISE] = LAYOUT(
+    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                          KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
     KC_TAB,  KC_UGUP, KC_UP,   KC_HASH, KC_HOME, KC_MSTP, KC_UNDS,                        KC_MPRV, KC_BTN1, KC_BTN3, KC_BTN2, KC_PSCR, KC_VOLU, KC_MAIL, \
     KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_MPLY, KC_BTN4,                        KC_MNXT, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_MUTE, KC_CALC, \
     KC_LSFT, KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_BTN5,                        KC_ENT,  KC_MH_L, KC_MH_U, KC_MH_D, KC_MH_R, KC_VOLD, KC_RSFT, \
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * +----------------------------------------------------------------------------------------------------------------------+
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
+   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * |      |      | BL ON|  BRTG|  INC |  DEC |      |                    |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
