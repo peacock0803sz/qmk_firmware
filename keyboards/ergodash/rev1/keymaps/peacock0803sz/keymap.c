646c9514ccebefb9ecf5d75501efbb6c07f63904
peacock <peacock0803sz@gmail.com>
2020-11-15 10:38:24 +0900

    :zap: Write my ergodash keymap

--- keyboards/ergodash/rev1/keymaps/peacock0803sz/keymap.c
+++ keyboards/ergodash/rev1/keymaps/peacock0803sz/keymap.c
@@ -25,19 +25,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |   [  |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |   -  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | GUI  |   A  |   S  |   D  |   F  |   G  |   -  |                    |   =  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  | Enter|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | EISU | EISU |  GUI |  Alt |||||||| Lower| Enter| Space||||||||Soace | Enter| Raise|||||||| Alt  |RCtrl | EISU | EISU |
+   * | EISU | EISU |  Ctrl|  Alt |||||||| Lower| Enter| Space||||||||Soace | Enter| Raise|||||||| Alt  |RCtrl | EISU | EISU |
    * +----------------------------------------------------------------------------------------------------------------------+
    */
   [_QWERTY] = LAYOUT( \
-    KC_ESC,    KC_1,      KC_2,    KC_3,    KC_4, KC_5,  KC_MINS,                        KC_EQL,  KC_6,  KC_7, KC_8,    KC_9,    KC_0,      KC_BSPC,  \
-    KC_TAB,    KC_Q,      KC_W,    KC_E,    KC_R, KC_T,  KC_LBRC,                        KC_RBRC, KC_Y,  KC_U, KC_I,    KC_O,    KC_P,      KC_BSLS,  \
-    KC_LCTL,   KC_A,      KC_S,    KC_D,    KC_F, KC_G,  KC_GRV,                         KC_GRV,  KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN,   KC_QUOT,  \
-    KC_LSFT,   KC_Z,      KC_X,    KC_C,    KC_V, KC_B,  KC_SPC,                         KC_ENT,  KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,  \
-    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,       LOWER, KC_SPC,  KC_ENT,        KC_SPC, KC_ENT,  RAISE,       KC_RALT, KC_RCTL, C(KC_SPC), C(KC_SPC) \
+    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4, KC_5,  KC_MINS,                        KC_EQL,  KC_6,  KC_7, KC_8,    KC_9,    KC_0,    KC_BSPC,  \
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R, KC_T,  KC_LBRC,                        KC_RBRC, KC_Y,  KC_U, KC_I,    KC_O,    KC_P,    KC_BSLS,  \
+    KC_LCTL,   KC_A,    KC_S,    KC_D,    KC_F, KC_G,  KC_GRV,                         KC_GRV,  KC_H,  KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V, KC_B,  KC_SPC,                         KC_ENT,  KC_N,  KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
+    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,     LOWER, KC_SPC,  KC_ENT,        KC_SPC, KC_ENT,  RAISE,       KC_RALT, KC_RGUI, KC_RCTL, C(KC_SPC) \
   ),
 
   /* Lower
@@ -54,11 +54,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * +----------------------------------------------------------------------------------------------------------------------+
    */
   [_LOWER] = LAYOUT(
-    KC_TILD,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,          \
-    KC_TAB,    KC_PGUP,   KC_UP,   KC_PGDN, KC_HOME, KC_MSTP, KC_BTN4,                          KC_MPRV, KC_BTN1, KC_BTN3, KC_BTN2, KC_PSCR, KC_VOLU,   RALT(KC_BSLASH), \
-    KC_CAPS,   KC_LEFT,   KC_DOWN, KC_RGHT, KC_END,  KC_MPLY, KC_BTN5,                          KC_MNXT, KC_MS_L, KC_MS_U, KC_MS_D, KC_MS_R, KC_MUTE,   KC_CALC,         \
-    KC_LSFT,   KC_UNDO,   KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_SPC,                           KC_ENT,  KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_VOLD,   KC_RSFT,         \
-    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,          LOWER,   KC_SPC,  KC_ENT,          KC_SPC, KC_ENT,  RAISE,            KC_RALT, KC_RCTL, C(KC_SPC), C(KC_SPC)        \
+    KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7,        KC_F8,   KC_F9,   KC_F10,        KC_F11,        KC_F12,        KC_DEL,          \
+    KC_TAB, KC_BTN1, KC_MS_U, KC_BTN2, KC_HOME, KC_VOLU, KC_BTN4,                 LCA(KC_UP),   KC_WH_U, KC_PGUP, KC_UP,         KC_PGDN,       KC_HOME,       RALT(KC_BSLASH), \
+    KC_LCTL,   KC_MS_L, KC_MS_D, KC_MS_R, KC_END,  KC_VOLD, KC_BTN5,                 LCA(KC_DOWN), KC_BTN3, KC_LEFT, KC_DOWN,       KC_RGHT,       KC_END,        RALT(KC_LBRC),   \
+    KC_LSFT,   KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_SPC,                  KC_ENT,       KC_WH_D, KC_MUTE, RALT(KC_COLN), RALT(KC_QUES), RALT(KC_RBRC), KC_RSFT,         \
+    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,          LOWER,   KC_SPC,  KC_ENT, KC_SPC, KC_ENT,       RAISE,            KC_RALT,       KC_RGUI,       KC_RCTL,       C(KC_SPC)        \
   ),
 
   /* Raise
@@ -74,12 +74,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | C-SPC| C-SPC|  GUI |  Alt |||||||| Lower| Enter| Space||||||||Soace | Enter| Raise|||||||| Alt  |RCtrl |C-SPC |C-SPC |
    * +----------------------------------------------------------------------------------------------------------------------+
    */
-[_RAISE] = LAYOUT(
-    KC_TILD,   KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,    KC_DEL,          \
-    KC_TAB,    KC_PGUP,   KC_UP,   KC_PGDN, KC_HOME, KC_MSTP, KC_BTN4,                          KC_MPRV, KC_BTN1, KC_BTN3, KC_BTN2, KC_PSCR, KC_VOLU,   RALT(KC_BSLASH), \
-    KC_CAPS,   KC_LEFT,   KC_DOWN, KC_RGHT, KC_END,  KC_MPLY, KC_BTN5,                          KC_MNXT, M(C(KC_LEFT)), M(C(KC_DOWN)), M(C(KC_UP)), M(C(KC_RIGHT)), KC_MUTE,   KC_CALC,         \
-    KC_LSFT,   KC_UNDO,   KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_SPC,                           KC_ENT,  KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, KC_VOLD,   KC_RSFT,         \
-    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,          LOWER,   KC_SPC,  KC_ENT,          KC_SPC, KC_ENT,  RAISE,            KC_RALT, KC_RCTL, C(KC_SPC), C(KC_SPC)        \
+
+  [_RAISE] = LAYOUT(
+    KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                   KC_F7,        KC_F8,   KC_F9,   KC_F10,        KC_F11,        KC_F12,        KC_DEL,          \
+    KC_TAB, KC_BTN1, KC_MS_U, KC_BTN2, KC_HOME, KC_VOLU, KC_BTN4,                 LCA(KC_UP),   KC_WH_U, KC_PGUP, KC_UP,         KC_PGDN,       KC_HOME,       RALT(KC_BSLASH), \
+    KC_LCTL,   KC_MS_L, KC_MS_D, KC_MS_R, KC_END,  KC_VOLD, KC_BTN5,                 LCA(KC_DOWN), KC_BTN3, KC_LEFT, KC_DOWN,       KC_RGHT,       KC_END,        RALT(KC_RBRC),   \
+    KC_LSFT,   KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_SPC,                  KC_ENT,       KC_WH_D, KC_MUTE, RALT(KC_COLN), RALT(KC_QUES), RALT(KC_RBRC), KC_RSFT,         \
+    C(KC_SPC), C(KC_SPC), KC_LGUI, KC_LALT,          LOWER,   KC_SPC,  KC_ENT, KC_SPC, KC_ENT,       RAISE,            KC_RALT,       KC_RGUI,       KC_RCTL,       C(KC_SPC)        \
   ),
 
   /* Adjust
